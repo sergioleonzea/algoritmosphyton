5c6cd8ef8395cf465b5f465b07dd25092c82a7c6
sergioleonzea <slmz25leon@gmail.com>
2020-10-08 21:08:25 -0500

    solucion ejercisio 2

---
+++ ejerci_2.py
@@ -0,0 +1,13 @@
+numero1=int(20)
+numero2=int(2)
+
+suma = numero1 + numero2
+print("el resultado de la suma es: " + str(suma))
+resta = numero1 - numero2
+print("el resultado de la resta es: " + str(resta))
+
+divi = numero1 / numero2
+print("el resultado de la divicion es: " + str(divi))
+
+multi = numero1 * numero2
+print("el resultado de la multiplicacion es: " + str(multi))
